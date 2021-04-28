adb80991ff8e562f3fd6cb3da3dbb634b7720759
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-04-28 21:21:32 +0800

    Add files via upload

---
+++ CommentData.java
@@ -0,0 +1,35 @@
+package page.page1;
+
+public class CommentData {
+
+        private int s_id;
+        private String course_code;
+        private String prof_name;
+        private String comment;
+
+        public CommentData() {}
+
+        public CommentData(int s_id, String course_code, String prof_name, String comment) {
+            this.s_id = s_id;
+            this.course_code = course_code;
+            this.prof_name = prof_name;
+            this.comment = comment;
+        }
+
+        public int getSid() {
+            return s_id;
+        }
+
+        public String getCourse_code() {
+            return course_code;
+        }
+
+        public void setProf_name() {
+            this.prof_name = prof_name;
+        }
+
+        public void setComment(String content) {
+            this.comment = content;
+        }
+
+}
