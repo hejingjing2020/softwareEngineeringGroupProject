0fd06f851474ac5691a437d1313fa55e0539863a
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-05-11 15:50:43 +0800

    教师端可以使用了

--- app/src/main/java/page/page1/courseEvaluation_prof.java
+++ app/src/main/java/page/page1/courseEvaluation_prof.java
@@ -5,16 +5,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
-import android.widget.SimpleAdapter;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,40 +26,33 @@ import java.util.logging.Handler;
 
 public class courseEvaluation_prof extends AppCompatActivity implements View.OnClickListener{
 
-    String TABLENAME = "Comment";
+    String TABLENAME = "iteminfo";
     Intent intent;
     byte[] imagedata;
     Bitmap imagebm;
     private EditText et_name;
-    private LinkedList<CommentData> mList;
+
     private myAdapter mAdapter;
-    private ListView mListView;
-    private Handler mHandler;
+    private android.widget.ListView lv;
+    public static LinkedList<CommentData> mList = new LinkedList<CommentData>();
 
-    /*
-    @Override
-    protected void onResume() {
-
-        super.onResume();
-
-        onCreate(null);
-
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mList = new LinkedList<CommentData>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_evaluation_prof);
+        this.lv = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<CommentData> adapter;
+        SearchView searchView;
 
+
+        //先setAdapter再notifyDataChanged
 
         DatabaseHelper database = new DatabaseHelper(this);
         final SQLiteDatabase db = database.getWritableDatabase();
-        ListView listView = (ListView)findViewById(R.id.listView);
-        mList = new LinkedList<CommentData>();
 
-        //mAdapter = new myAdapter(mList, courseEvaluation_prof.this);
-        //这里在每次更新数据时刷新listView
-        //listView.setAdapter(mAdapter);
+
 
 
         Map<String, Object> item;  // 列表项内容用Map存储
@@ -67,21 +61,32 @@ public class courseEvaluation_prof extends AppCompatActivity implements View.OnC
 
 
         if (cursor.moveToFirst()){
-            while (!cursor.isAfterLast()){
-                item = new HashMap<String, Object>();  // 为列表项赋值
-                item.put("s_id",cursor.getInt(0));
-                item.put("course_code",cursor.getString(1));
-                item.put("prof_name",cursor.getString(2));
-                item.put("comment",cursor.getString(3));
-                //imagedata = cursor.getBlob(6);
-                //item.put("image",imagebm);
+            while (!cursor.isAfterLast()) {
+                CommentData cd = new CommentData();  // 为列表项赋值
+                cd.setS_id(cursor.getInt(0));
+                cd.setCourse_code(cursor.getString(1));
+                cd.setProf_name(cursor.getString(2));
+                cd.setComment(cursor.getString(3));
+                cd.setCid();
+                mList.add(cd);
                 cursor.moveToNext();
-                data.add(item); // 加入到列表中
             }
         }
+        mAdapter = new myAdapter<CommentData>(mList, courseEvaluation_prof.this, R.layout.listitem) {
+            @Override
+            public void convertView(ViewHolder holder, CommentData Data) {
+                holder.set(R.id.title, Data.getCourse_code())
+                        .set(R.id.kind, Data.getProf())
+                        .set(R.id.info, Data.getComment());
+            }
+        };
+        //这里在每次更新数据时刷新listView
+        lv.setAdapter(mAdapter);//listView里应该是mList的内容
+        mAdapter.notifyDataSetChanged();
+
         // 使用SimpleAdapter布局 listview
         // listitem.xml
-        //myAdapter mAdapter = new myAdapter();
+        //为什么不能用simpleAdapter？因为它的信息是hard-coded在listitem.xml里的
 
         ImageView kind1 = (ImageView) findViewById(R.id.kind1);
         kind1.setOnClickListener(this);
@@ -91,25 +96,58 @@ public class courseEvaluation_prof extends AppCompatActivity implements View.OnC
         kind3.setOnClickListener(this);
         ImageView kind4 = (ImageView) findViewById(R.id.kind4);
         kind4.setOnClickListener(this);
+        ImageView kind5 = (ImageView) findViewById(R.id.kind5);
+        kind5.setOnClickListener(this);
+
 
 
 
         // 为列表项设置监听器
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
                 intent = new Intent(courseEvaluation_prof.this, item_info.class);
-                intent.putExtra("id", data.get(position).get("id").toString()); // 获取该列表项的key为id的键值，即商品的id，将其储存在Bundle传递给打开的页面
+                intent.putExtra("cid", mList.get(position).getCid()); // 获取该列表项的key为id的键值，即商品的id，将其储存在Bundle传递给打开的页面
                 startActivity(intent);
             }
         });
 
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                Cursor search_cursor = db.query(TABLENAME,null,"course_code like ? OR prof_name like ?",new String[]{"%"+query+"%", "%"+query+"%"},null,null,null,null); // 数据库查询
+                //Cursor search_cursor = db.rawQuery("SELECT * FROM iteminfo where course_code like '%?%' OR prof_name like '%?%'", new String[]{query, query});
+                mList.clear();
+                if (search_cursor.moveToFirst()){
+                    while (!search_cursor.isAfterLast()) {
+                        CommentData cd = new CommentData();  // 为列表项赋值
+                        cd.setS_id(search_cursor.getInt(0));
+                        cd.setCourse_code(search_cursor.getString(1));
+                        cd.setProf_name(search_cursor.getString(2));
+                        cd.setComment(search_cursor.getString(3));
+                        cd.setCid();
+                        mList.add(cd);
+                        search_cursor.moveToNext();
+                    }
+                }
+                mAdapter.notifyDataSetChanged();
+                return true;
+            }
+
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
         RadioButton btn1 = (RadioButton)findViewById(R.id.button_main_page);
-        RadioButton btn2 = (RadioButton)findViewById(R.id.button_post_comment);
         RadioButton btn3 = (RadioButton)findViewById(R.id.button_self_center);
         btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
     }
     private ArrayList<Map<String, Object>> getData(){
@@ -158,3 +196,5 @@ public class courseEvaluation_prof extends AppCompatActivity implements View.OnC
         }
     }
 }
+
+
