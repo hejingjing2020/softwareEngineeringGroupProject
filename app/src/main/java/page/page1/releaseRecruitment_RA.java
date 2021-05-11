b54ca0e9f6a4c241002de956d6be0a676cfd7831
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-05-11 20:34:59 +0800

    Update releaseRecruitment_RA.java

--- app/src/main/java/page/page1/releaseRecruitment_RA.java
+++ app/src/main/java/page/page1/releaseRecruitment_RA.java
@@ -1,53 +1,72 @@
 package page.page1;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static page.page1.LoginMainActivity.post_userid;
+
+
 
 
 public class releaseRecruitment_RA extends AppCompatActivity {
     private static final byte REQUEST_SYSTEM_PIC = 10;
     //private static PreparedStatement dbHelper;
-    public static DatabaseHelper dbHelper;
-    static final SQLiteDatabase db = dbHelper.getReadableDatabase();
+
     private Spinner sp;
+    private ImageButton imageButton;
 
     public releaseRecruitment_RA() throws SQLException {
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_release_recruitment__r);
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        setContentView(R.layout.activity_release_recruitment__u_s_t_f);
         final SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss ");
         //dbHelper=new MyDatabaseHelper(this,"1600802129.db",null,1);
-        dbHelper = new DatabaseHelper(this);
-        final SQLiteDatabase db = dbHelper.getReadableDatabase();
+
         String[] ctype = new String[]{"生活用品", "学习用品", "电子产品", "体育用品"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ctype);  //创建一个数组适配器
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
 
-        Button submit=(Button)findViewById(R.id.release_comment);
-        submit.setOnClickListener(new View.OnClickListener() {
+
+        Button fabu=(Button)findViewById(R.id.release_comment);
+        fabu.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
@@ -94,5 +113,16 @@ public class releaseRecruitment_RA extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
 }
-
