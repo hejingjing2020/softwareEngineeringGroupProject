7b3f5d8c11d2a7c51793eab5d6ea06e24a29fcdc
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-05-11 15:49:32 +0800

    根据role跳转

--- app/src/main/java/page/page1/main_page.java
+++ app/src/main/java/page/page1/main_page.java
@@ -1,49 +1,26 @@
 
 package page.page1;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class main_page extends AppCompatActivity implements View.OnClickListener{
-    Intent intent;
-    private Button button_4;
-    private Button button_5;
-    private Button button_6;
-    private Button course_evaluation_page;
-    private Button recruit_page;
-    String TABLENAME = "commentInfo";
+
+    private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
         DatabaseHelper database = new DatabaseHelper(this);
         final SQLiteDatabase db = database.getWritableDatabase();
+        intent = getIntent();
 
 
         Button btn1 = (Button)findViewById(R.id.course_evaluation_page);
@@ -55,13 +32,26 @@ public class main_page extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+        String role = intent.getStringExtra("role");
         switch (v.getId()) {
             case R.id.course_evaluation_page:
-                Intent KindIntent1 = new Intent(main_page.this, courseEvaluation.class);
+                Intent KindIntent1;
+                if (role=="Student") {
+                    KindIntent1 = new Intent(main_page.this, courseEvaluation.class);
+                }
+                else {
+                    KindIntent1 = new Intent(main_page.this, courseEvaluation_prof.class);
+                }
                 startActivity(KindIntent1);
                 break;
             case R.id.recruit_page:
-                Intent KindIntent2 = new Intent(main_page.this, recruitment.class);
+                Intent KindIntent2;
+                if (role=="Student") {
+                    KindIntent2 = new Intent(main_page.this, recruitment.class);
+                }
+                else {
+                    KindIntent2 = new Intent(main_page.this, recruitment_prof.class);
+                }
                 startActivity(KindIntent2);
                 break;
             case R.id.button_main_page:
