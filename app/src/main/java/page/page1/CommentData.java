41ad61743e7a83ca348c5da96ddabccc7600b1f6
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-05-11 18:08:06 +0800

    Update CommentData.java

--- app/src/main/java/page/page1/CommentData.java
+++ app/src/main/java/page/page1/CommentData.java
@@ -7,22 +7,28 @@ public class CommentData {
         private String prof_name;
         private String comment;
         private String cid;
+        private byte[] image;
 
         public CommentData() {}
 
-        public CommentData(int s_id, String course_code, String prof_name, String comment) {
+        public CommentData(int s_id, String course_code, String prof_name, String comment, byte[] image) {
             this.cid = String.valueOf(s_id) + course_code;
             this.s_id = s_id;
             this.course_code = course_code;
             this.prof_name = prof_name;
             this.comment = comment;
+            this.image = image;
         }
         public String getCid() { return cid; }
         public void setCid() { this.cid = String.valueOf(s_id)+course_code; }
+
         public void setS_id(int sid) { this.s_id = sid; }
         public int getSid() {
             return s_id;
         }
+
+        public void setImage(byte[] image) { this.image = image; }
+        public byte[] getImage() { return image; }
 
         public void setCourse_code(String course_code) {this.course_code = course_code;}
         public String getCourse_code() {
@@ -42,3 +48,4 @@ public class CommentData {
 
 
 }
+
