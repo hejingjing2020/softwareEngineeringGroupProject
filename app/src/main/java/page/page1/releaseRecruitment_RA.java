efb7ce2a4bfa2f9dc01f3cf9c701c1265e265678
hejingjing2020 <47182731+hejingjing2020@users.noreply.github.com>
2021-05-11 20:32:30 +0800

    Update releaseRecruitment_RA.java

--- app/src/main/java/page/page1/releaseRecruitment_RA.java
+++ app/src/main/java/page/page1/releaseRecruitment_RA.java
@@ -1,44 +1,25 @@
 package page.page1;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static page.page1.LoginMainActivity.post_userid;
-
-
 
 
 public class releaseRecruitment_RA extends AppCompatActivity {
@@ -47,8 +28,6 @@ public class releaseRecruitment_RA extends AppCompatActivity {
     public static DatabaseHelper dbHelper;
     static final SQLiteDatabase db = dbHelper.getReadableDatabase();
     private Spinner sp;
-    private ImageButton imageButton;
-    private byte[] image;
 
     public releaseRecruitment_RA() throws SQLException {
     }
@@ -67,39 +46,10 @@ public class releaseRecruitment_RA extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ctype);  //创建一个数组适配器
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
 
-
-        imageButton=(ImageButton)findViewById(R.id.m1_image);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(releaseRecruitment_RA.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(releaseRecruitment_RA.this, new
-                            String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                } else {
-                    //打开系统相册
-                    Intent intent = new Intent(Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(intent, 1);
-                }
-
-            }
-        });
-
         Button submit=(Button)findViewById(R.id.release_comment);
         submit.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
-            /*
-              "(rid varchar(20) primary key," +
-                "uid varchar(20)," +
-                "title varchar(50) not null," +
-                "email varchar(15)," +
-                "salary varchar(15)," +
-                "type varchar(5)," +
-                "decription varchar(20))");
-
-             */
             public void onClick(View v) {
                 EditText title=(EditText)findViewById(R.id.m1_title);
                 EditText email=(EditText)findViewById(R.id.m1_email);
@@ -116,8 +66,6 @@ public class releaseRecruitment_RA extends AppCompatActivity {
                 values.put("type", "RA");
                 values.put("description", description.getText().toString());
                 db.insert("recruitment",null,values);
-                // int s_id, String course_code, String prof_name, String comment
-                //(courseEvaluation.mList).add(new CommentData(Integer.valueOf(post_userid), title.getText().toString(), prof.getText().toString(), comment.getText().toString()));
                 Intent intent=new Intent(releaseRecruitment_RA.this, courseEvaluation.class);
                 Toast.makeText(getApplicationContext(), "Submitted", Toast.LENGTH_SHORT).show();
 
@@ -145,42 +93,6 @@ public class releaseRecruitment_RA extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //获取图片路径
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumns = {MediaStore.Images.Media.DATA};
-            Cursor c = getContentResolver().query(selectedImage, filePathColumns, null, null, null);
-            c.moveToFirst();
-            int columnIndex = c.getColumnIndex(filePathColumns[0]);
-            String imagePath = c.getString(columnIndex);
-            showImage(imagePath);
-            c.close();
-        }
-    }
 
-    //加载图片
-    private void showImage(String imaePath) {
-        Bitmap bm = BitmapFactory.decodeFile(imaePath);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        image = baos.toByteArray();
-        imageButton.setImageBitmap(bm);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-        }
-    }
 }
 
